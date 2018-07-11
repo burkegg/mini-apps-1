e82fce487da3ef709e759655d938eddfdaca0f2f
burkegg <burkegreen@gmail.com>
2018-07-10 17:37:25 -0700

    Mostly working client and server.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,52 +1,49 @@
 // this is where code or the ajax calls 
 //from the client will go.
 
+
+
 $(document).ready(function(){
-  // MAKE AJAX HAPPEN!
 
-// $.ajax({
-//   type: "POST",
-//   url: 'localhost:3000',
-//   data: {"firstName":"Beth","lastName":"Johnson","county":"San Francisco","city":"San Francisco","role":"Broker/Sales Person","sales":7500000,"children":[]},
-//   dataType: 'application/json',
-// });
-  
-// $.ajax({
-//   type: "GET",
-//   url: 'http://localhost:3000/data',
-//   //datatype: ''    // the type you expect back.  it might guess for me.
-//   success: function(error, response) {console.log(error);}
-// });
 
-$.ajax({
-  type: "POST",
-  url: 'http://localhost:3000/data',
-  datatype: "application/json",
-  contentType: "application/json",
-  data: '{"firstName":"RRRRRR","lastName":"GRRRRERERETson","county":"WHEREEEEEE","city":"San Francisco","role":"Broker/Sales Person","sales":7500000,"children":[]}',
+  var getAll = function(){
+    $.ajax({
+      type: "GET",
+      url: 'http://localhost:3000/data',
+      //datatype: ''    // the type you expect back.  it might guess for me.
+      success: function(response) {
+        console.log(response);
+        currentTasks = response;
+        // post to DOM
+        $('#csv').text(currentTasks);
+      }
+    });
+  }
 
-})
-// }
-console.log('inside js');
+  var postIt = function(jsonData) {
+    $.ajax({
+      type: "POST",
+      url: 'http://localhost:3000/data',
+      datatype: "application/json",
+      contentType: "application/json",
+      data: jsonData,
+    })
+  };
+  var currentTasks = '';
+  getAll();
+
+
+  $("form").on("submit", function(event) {
+    event.preventDefault(); 
+    //console.log(event);
+    let dataToPost = $('#reports').val()
+    postIt(dataToPost);
+    getAll();
+
+  });
+
 });
 
-$.ajax({
-  type: "GET",
-  url: 'http://localhost:3000/data',
-  //datatype: ''    // the type you expect back.  it might guess for me.
-  success: function(error, response) {console.log(error);}
-});
-// $.get("localhost:3000/data", function(){
-//   console.log('got?');
-// }, 'text');
 
-
-// $.post("localhost:3000/data", {"firstName":"Beth","lastName":"Johnson","county":"San Francisco","city":"San Francisco","role":"Broker/Sales Person","sales":7500000,"children":[]}, function() {
-//   console.log('posted');
-// }, 'json');
-  
-// });
-
-// {"firstName":"Beth","lastName":"Johnson","county":"San Francisco","city":"San Francisco","role":"Broker/Sales Person","sales":7500000,"children":[]}
 
  

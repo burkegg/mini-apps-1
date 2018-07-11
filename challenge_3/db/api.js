455036ea752959f762b9666dc17c29437b7e9b94
burkegg <burkegreen@gmail.com>
2018-07-11 13:22:50 -0700

    Working SIMPLEST POSSIBLE database.  Working database mgr.  Working database api.

--- challenge_3/db/api.js
+++ challenge_3/db/api.js
@@ -1,1 +1,14 @@
-const db = require('dbmanager');
+const db = require('./dbmanager');
+var someParams = ['SOMENAME', 'anemeail', 'somepass', 'someadd1', 'secondline', 'thecity', 'thestate', 43324, '5101231234', '44444444444', '12/01/21', 123, 242424];
+
+var addEntry = db.addAccount(someParams);
+
+
+// insert and select from database from here.
+
+var getAll = db.allFromDB;
+
+module.exports = {
+  addEntry,
+  getAll,
+}
